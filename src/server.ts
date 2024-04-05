1eba3f1895780af2988211aa5d624f3c3f850fba
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2024-04-04 22:31:35 -0300

    Criado nova rota get-attendee-badge

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { createEventRoute } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-events";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 
 
@@ -14,6 +15,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEventRoute)
 app.register(registerForEvent)
 app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
