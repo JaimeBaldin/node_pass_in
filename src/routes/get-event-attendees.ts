b1afe821497ffa2c0e182d3ae8a5b3751d05e1d4
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2024-04-07 04:59:42 -0300

    Feito ajuste na rota de buscar participantes, para que seja realizado uma busca na tabela

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -13,14 +13,25 @@ export async function getEventAttendees(app: FastifyInstance) {
                 }),
 
                 querystring: z.object({
+                    query: z.string().nullish().default(''),
                     pageIndex: z.string().nullable().default('0').transform(Number),
                 }),
 
-                response:{},
+                response:{
+                    200: z.object({
+                        attendees: z.array(z.object({
+                            id: z.number(),
+                            name: z.string(),
+                            email: z.string().email(),
+                            createdAt: z.date(),
+                            checkedInAt: z.date().nullable()
+                        }))
+                    })
+                },
             }
         }, async (request, reply) =>{
            const { eventId } = request.params
-           const { pageIndex } = request.query
+           const { pageIndex, query } = request.query
 
            const attendees = await prisma.attendee.findMany({
             select:{
@@ -34,11 +45,19 @@ export async function getEventAttendees(app: FastifyInstance) {
                     }
                 }
             },
-            where: {
+            where: query ? {
+                eventId,
+                name: {
+                    contains: query,
+                }
+            } : {
                     eventId,
                 },
                 take: 10,
                 skip: pageIndex * 10,
+                orderBy: {
+                    createdAt: 'desc'
+                }
            })
 
            return reply.send({
@@ -48,7 +67,7 @@ export async function getEventAttendees(app: FastifyInstance) {
                         name: attendee.name,
                         email: attendee.email,
                         createdAt: attendee.createdAt,
-                        checkedInAt: attendee.checkIn?.createdAt
+                        checkedInAt: attendee.checkIn?.createdAt ?? null
                     }
                 })
            })
